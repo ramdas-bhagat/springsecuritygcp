8927a76af1b9aa68aa36fb46c5dca76b027998fe
ramdas.bhagat <ramdas.bhagat@datametica.com>
2025-01-10 10:16:17 +0530

    Inital code commit

---
+++ src/main/java/com/test/springsecuritygcp/SpringsecuritygcpApplication.java
@@ -0,0 +1,13 @@
+package com.test.springsecuritygcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritygcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritygcpApplication.class, args);
+	}
+
+}
